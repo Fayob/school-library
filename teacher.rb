86168735e454561caef33f995efc1ac2ab41348b
fayob <adedejifavour6@gmail.com>
2022-12-22 09:35:45 +0100

    Refactor teacher class

--- teacher.rb
+++ teacher.rb
@@ -1,9 +1,11 @@
 require_relative 'person'
 
 class Teacher < Person
-  def initialize(age, specialization, name = 'Unknown', parent_permission: true)
-    super(age, name, parent_permission)
+  attr_reader :specialization
+
+  def initialize(age, specialization, name, parent_permission)
     @specialization = specialization
+    super(age, name, parent_permission)
   end
 
   def can_use_services?
