5eb9e733567379b43d2bd4eafc227d98ac316fcf
fayob <adedejifavour6@gmail.com>
2023-01-03 19:07:18 +0100

    Accept more parameter in constructor

--- student.rb
+++ student.rb
@@ -3,8 +3,8 @@ require_relative 'person'
 class Student < Person
   attr_reader :classroom
 
-  def initialize(age, classroom, name, parent_permission)
-    super(age, name, parent_permission)
+  def initialize(age, classroom, name, parent_permission, id)
+    super(age, name, parent_permission, id)
     @classroom = classroom
   end
 
