68095596e46d7182f3e96859fbba3fd32b3b4151
fayob <adedejifavour6@gmail.com>
2023-01-03 19:08:13 +0100

    Add id as an argument

--- teacher.rb
+++ teacher.rb
@@ -3,9 +3,9 @@ require_relative 'person'
 class Teacher < Person
   attr_reader :specialization
 
-  def initialize(age, specialization, name, parent_permission)
+  def initialize(age, specialization, name, parent_permission, id)
     @specialization = specialization
-    super(age, name, parent_permission)
+    super(age, name, parent_permission, id)
   end
 
   def can_use_services?
