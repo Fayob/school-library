3207f424649ddbbbccf5d2727edda987d39b2d6e
fayob <adedejifavour6@gmail.com>
2023-01-04 19:22:35 +0100

    Fix rubocop error

--- tests/all.rb
+++ tests/all.rb
@@ -1,1 +1,1 @@
-Dir[File.dirname(File.absolute_path(__FILE__)) + '/**/*.spec.rb'].each {|file| require file }
+Dir["#{File.dirname(File.absolute_path(__FILE__))}/**/*.spec.rb"].sort.each { |file| require file }
